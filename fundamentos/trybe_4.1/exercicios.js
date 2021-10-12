69ac124758075d34cc5d136a6e7d3c5d028984c7
Renato <renatoft89@gmail.com>
2021-10-12 17:55:36 -0300

    exercicio 2 3 5

--- fundamentos/trybe_4.1/exercicios.js
+++ fundamentos/trybe_4.1/exercicios.js
@@ -32,3 +32,48 @@ console.log(numero1)
 else{
   console.log(numero2);
 }
+
+//exercicio-3
+
+const n1 = 50;
+
+const n2 = 40;
+
+const n3 = 2;
+
+if(n1>n2&&n1>n3)
+  console.log(n1)
+else if(n2>n1&&n2>n3)
+  console.log(n2)
+else{
+  console.log(n3);
+}  
+
+//exercicio-4
+const numero = -14;
+
+if( numero>0)
+console.log("positive")
+else{
+  console.log("negativo");
+}
+
+//exercicio-5
+
+const ang1 = 30;
+
+const ang2 = 60;
+
+const ang3 = 90;
+
+
+if (ang1 < 0 || ang2 < 0 || ang3 < 0) {
+  console.log("Erro");
+
+} else if ((ang1 + ang2 + ang3) == 180) {
+  console.log("true");
+
+} else {
+  console.log("false");
+}
+  
