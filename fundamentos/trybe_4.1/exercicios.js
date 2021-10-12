1cc9d096887cddae5010ff0f721dc339e18a46f4
Renato <renatoft89@gmail.com>
2021-10-12 16:24:42 -0300

    exercicio 1 e 2

---
+++ fundamentos/trybe_4.1/exercicios.js
@@ -0,0 +1,34 @@
+//exercicio-1
+
+const operador = ('*');
+
+var num1 = 6;
+var num2 = 5;
+
+if (operador == '+'){
+  console.log(num1+num2);
+}
+
+else if (operador == '-'){
+  console.log(num1-num2);
+}
+else if (operador == '/'){
+  console.log(num1/num2);
+}
+else if (operador == '*'){
+  console.log(num1*num2);
+}
+else {
+  console.log('caractere invalido');
+}
+
+//exercicio-2
+
+const numero1 = 14;
+const numero2 = 15;
+
+if( numero1>numero2)
+console.log(numero1)
+else{
+  console.log(numero2);
+}
