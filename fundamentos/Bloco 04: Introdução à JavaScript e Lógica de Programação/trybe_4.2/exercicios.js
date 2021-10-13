08360a2c24a3f39cb55b54ca86bf97c712601781
Renato <renatoft89@gmail.com>
2021-10-13 19:02:43 -0300

    exercicio 1 e 2 arrays

---
+++ fundamentos/Bloco 04: Introdução à JavaScript e Lógica de Programação/trybe_4.2/exercicios.js
@@ -0,0 +1,14 @@
+//exercicio-1
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//exercicios-2
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log (menu.length)
+console.log(indexOfPortfolio);
---
+++ fundamentos/Bloco 04: Introdução à JavaScript e Lógica de Programação/trybe_4.2/mega-sena.js
@@ -0,0 +1,5 @@
+let meusNumeros =  [8, 19, 21, 35, 44, 46];
+let numerosSorteados =  Math.ceil(Math.random(6)*60);
+
+
+console.log(numerosSorteados)
