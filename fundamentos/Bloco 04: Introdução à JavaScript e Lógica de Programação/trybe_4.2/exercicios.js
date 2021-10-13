40f9a28ce1a0fba78d4e0f9ff1ba264ab60eeb08
Renato <renatoft89@gmail.com>
2021-10-13 20:35:00 -0300

    Exercicio-5 atividade pratica

--- fundamentos/Bloco 04: Introdução à JavaScript e Lógica de Programação/trybe_4.2/exercicios.js
+++ fundamentos/Bloco 04: Introdução à JavaScript e Lógica de Programação/trybe_4.2/exercicios.js
@@ -25,6 +25,64 @@ console.log(menu);
 
 let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-for (i=0; i<groceryList.length; i++){
+for (let i=0; i<groceryList.length; i++){
   console.log(groceryList[i]);
 }
+
+//exercicio-5
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for( let lista of names){
+  console.log(lista);
+}
+
+//APROFUNDE SEUS CONHECIMENTOS EXERCICIO-1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+  for ( let index=0; index < numbers.length; index++ ){
+    console.log(numbers[index]);
+  }
+
+//EXERCICIO-2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for ( let index=0 ; index < numbers.length ; index++ ){
+  soma += (numbers[index]);
+}
+  console.log(soma);
+
+  //EXERCICIO-3
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+  for ( let index=0 ; index < numbers.length ; index++ ){
+    soma += numbers[index];
+    
+  }
+    let media = soma / numbers.length;
+    console.log(media)
+//EXERCICIO-4
+
+  if (media>20){
+    console.log("Media é maior que 20");
+  }else{
+    console.log("Valor menor ou igual a 20")
+  }
+
+  //EXERCICIO-5
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior = 0;
+
+for (let index=0; index<numbers.length; index++){
+  if (numbers[index]>maior){
+    maior = numbers[index];
+    
+  }
+}
+console.log(maior);
