b802298e977025d2ffb123a3911000fb83a3b9b1
Renato <renatoft89@gmail.com>
2021-10-15 15:48:13 -0300

    exercicio5

---
+++ fundamentos/Bloco 04: Introdução à JavaScript e Lógica de Programação/trybe_4.4/exercicios4.4.js
@@ -0,0 +1,48 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+//1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+
+console.log('Bem Vinda ',info.personagem);
+
+//2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+
+info.recorrente = 'Sim';
+
+console.log(info);
+
+//3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+for( index in info){
+  console.log(index)
+}
+
+//4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+for( index in info){
+  console.log(info[index])
+}
+
+//5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'r CoChristmas on Bear Mountain, Dell's Foulor Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (index in info){
+  if(index === 'recorrente'&&
+  info[index]=== 'Sim' &&
+  info2[index] === 'Sim'
+  ){
+    console.log('Ambos Recorrentes')
+  }else{
+  
+    console.log(info[index], 'e', info2[index]);
+  }
+}
