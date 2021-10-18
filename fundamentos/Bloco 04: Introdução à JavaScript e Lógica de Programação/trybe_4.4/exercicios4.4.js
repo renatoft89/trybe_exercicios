70edb5c5ee36a2d59cb30a3e87b7821cb3e6d7d4
Renato <renatoft89@gmail.com>
2021-10-18 13:17:56 -0300

    exercicio-4

--- fundamentos/Bloco 04: Introdução à JavaScript e Lógica de Programação/trybe_4.4/exercicios4.4.js
+++ fundamentos/Bloco 04: Introdução à JavaScript e Lógica de Programação/trybe_4.4/exercicios4.4.js
@@ -115,3 +115,19 @@ function menorValor(lista){
   
 }
 console.log(menorValor([2, 4, 6, 7, 10, 0, -3]));
+
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
+// Valor esperado no retorno da função: Fernanda .
+
+function contaCaracter(nomes){
+  let contador = nomes[0];
+  for (let key in nomes) {    
+    if (contador.length < nomes[key].length){
+      contador = nomes[key];
+    }
+  }
+  return contador;
+}
+
+console.log(contaCaracter(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
