c41d83e71bbc2a764f5aa233425eec8f2fc3e487
Renato <renatoft89@gmail.com>
2021-11-17 14:56:00 -0300

    fixando ok

--- fundamentos/Bloco 08: Higher Order Functions do JavaScript ES6/trybe_8.1/fixando.js
+++ fundamentos/Bloco 08: Higher Order Functions do JavaScript ES6/trybe_8.1/fixando.js
@@ -1,2 +1,18 @@
 // 1 - Crie uma função que retorne a string 'Acordando!!' ;
-const manha = () => 'Acordando';
+const manha = () => 'Acordando!!';
+
+//2 - Crie outra função que retorne a string 'Bora tomar café!!' ;
+const refeicaoManha = () => 'Bora tomar café!!';
+
+//3 - Crie mais uma função que retorne a string 'Partiu dormir!!' ;
+const noite = () => 'Partiu dormir!!';
+
+// 4 - Agora desenvolva uma HOF chamada doingThings e configure esta função para que imprima no console o resultado da execução das funções que você criou nos exemplos anteriores. Exemplo:
+const doingThings = (callback) => {
+  const result = callback();
+  console.log(result);
+};
+
+doingThings(manha);
+doingThings(refeicaoManha);
+doingThings(noite);
