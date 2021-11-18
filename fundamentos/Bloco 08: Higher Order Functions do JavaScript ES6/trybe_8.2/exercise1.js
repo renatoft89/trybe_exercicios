a8446729c70564f645f8b42defad27ab03b7bdca
Renato <renatoft89@gmail.com>
2021-11-18 19:24:32 -0300

    exercicio-3

--- fundamentos/Bloco 08: Higher Order Functions do JavaScript ES6/trybe_8.2/exercise1.js
+++ fundamentos/Bloco 08: Higher Order Functions do JavaScript ES6/trybe_8.2/exercise1.js
@@ -82,3 +82,24 @@ function smallerName() {
 }
 
 smallerName();
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  // escreva seu código aqui
+  let retorno = books.find((book) => book.name.length === 26);
+  if (expectedResult.name === retorno.name) {
+    console.log(retorno);
+  }
+  
+}
+getNamedBook();
