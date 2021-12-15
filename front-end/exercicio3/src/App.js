507770bfd05d17aa1b72179da7003c61fc7c7226
Renato Alves <renatoft89@gmail.com>
2021-12-15 19:16:09 -0300

    exercicio3

---
+++ front-end/exercicio3/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Content from './Content';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <Content />
+      </header>
+    </div>
+  );
+}
+
+export default App;
