77ab03f33add584c223ba90145decb3bd233cda0
Renato <renatoft89@gmail.com>
2022-04-19 15:17:57 -0300

    features22.4 - estruturando API

--- Bloco 22 - Introdução ao desenvolvimento Web com Node.js/trybe22.4/hello-express/index.js
+++ Bloco 22 - Introdução ao desenvolvimento Web com Node.js/trybe22.4/hello-express/index.js
@@ -1,13 +1,20 @@
+/* index.js */
 const express = require('express');
+const app = express();
 
-const app = express(); // 1
+const drinks = [
+  { id: 6, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 1, name: 'Água Mineral 500 ml', price: 5.0 },
+];
 
-app.get('/hello', handleHelloWorldRequest); // 2
+app.get('/drinks', function (req, res) {
+  res.json(drinks);
+});
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
-}); // 3
-
-function handleHelloWorldRequest(req, res) {
-  res.status(200).send('Hello World!'); // 4
-}
+});
