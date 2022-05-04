54708920b82e8b52a76471e4ad8d53bea2c41365
Renato <renatoft89@gmail.com>
2022-05-04 08:53:08 -0300

    ex pratico

--- Bloco 23 Node.js Camada de Serviço e Arquitetura/trybe_23.1/index.js
+++ Bloco 23 Node.js Camada de Serviço e Arquitetura/trybe_23.1/index.js
@@ -1,19 +1,25 @@
-    // index.js
+// index.js
 
-    const express = require('express');
+const express = require('express');
 
-    const app = express();
+const app = express();
 
-    const Author = require('./models/Author');
+const Author = require('./models/Author');
 
-    app.get('/authors', async (_req, res) => {
-      const authors = await Author.getAll();
+app.get('/authors', async (_req, res) => {
+const authors = await Author.getAll();
 
-      res.status(200).json(authors);
-  });
+res.status(200).json(authors);
+});
 
-    const PORT = process.env.PORT || 3000;
+app.post('/user', async (req, res) => {
+  res.status(200).json({ message: 'sucesso'})
+})
 
-    app.listen(PORT, () => {
-        console.log(`Ouvindo a porta ${PORT}`);
-    });
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+});
